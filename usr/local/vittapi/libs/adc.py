e3946a55346e32caaa168529a72eb857a900b109
Nicolas Godeau <63402663+Nixoals@users.noreply.github.com>
2023-08-16 13:49:27 +0200

    grove_temp

--- usr/local/vittapi/libs/adc.py
+++ usr/local/vittapi/libs/adc.py
@@ -10,7 +10,10 @@ class ADC():
     def read(self):
         value = self.adc.read(self.channel)
         return value
-        
+    
+    def read_analog(self):
+        value = self.adc.read_voltage(self.channel)
+        return value
 
 
 # def main():
