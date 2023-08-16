750af8bad02749cc198ea9aed28eef3979076f10
Nicolas Godeau <63402663+Nixoals@users.noreply.github.com>
2023-08-16 13:29:34 +0200

    grove_temp

--- usr/local/vittapi/libs/grove_temperature.py
+++ usr/local/vittapi/libs/grove_temperature.py
@@ -2,30 +2,16 @@ import math
 import time
 from grove.adc import ADC
 
-class GroveTemperature:
+class ADC():
     def __init__(self, channel):
         self.channel = channel
         self.adc = ADC()
 
-    def read(self, unit='c'):
+    def read(self):
         value = self.adc.read(self.channel)
-        resistance = (float)(1023 - value) * 10000 / value
+
+        return value
         
-        # Coefficients pour le thermistor NTC 10K
-        A = 1.009249522e-03
-        B = 2.378405444e-04
-        C = 2.019202697e-07
-        
-        # Calculez la température en Kelvin
-        temperature_in_kelvin = 1 / (A + B * math.log(resistance) + C * math.pow(math.log(resistance), 3))
-        
-        # Convertir en degrés Celsius
-        temperature = temperature_in_kelvin - 273.15
-        if unit.lower() == 'fahrenheit':
-            temperature = temperature * 1.8 + 32
-        elif unit.lower() == 'kelvin':
-            temperature = temperature_in_kelvin
-        return temperature
 
 
 # def main():
