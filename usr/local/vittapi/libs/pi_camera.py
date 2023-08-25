9f0764796b59a41d3f23dfe462a872c79d08585c
Nicolas Godeau <63402663+Nixoals@users.noreply.github.com>
2023-08-25 15:47:12 +0200

    picamera

--- usr/local/vittapi/libs/pi_camera.py
+++ usr/local/vittapi/libs/pi_camera.py
@@ -5,22 +5,25 @@ import time
 import os
 
 
-
 class Camera(object):
     def __init__(self):
         self.camera = Picamera2()
         self.camera_config = self.camera.create_preview_configuration()
         self.camera.configure(self.camera_config)
-        #self.camera.start_preview(Preview.DRM)
+        # self.camera.start_preview(Preview.DRM)
 
-    
     def get_frame(self):
         self.camera.start()
         time.sleep(2)
         data = io.BytesIO()
         image = self.camera.capture_buffer()
         self.camera.stop()
-        image_base64 = base64.b64encode(image)
+        # Décode en chaîne de caractères pour l'écriture dans un fichier texte
+        image_base64 = base64.b64encode(image).decode("utf-8")
+
+        # Sauvegarde de l'image base64 dans un fichier
+        with open("image_base64.txt", "w") as f:
+            f.write(image_base64)
+
         print(f"###BEGIN_IMAGE###\n{image_base64}\n###END_IMAGE###")
         return None
-
